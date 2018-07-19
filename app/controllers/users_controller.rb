0079063f7859ea0f9bb306a6bc7426779dd0b54f
Lucas Padua <lucas.p2222@gmail.ciom>
2018-07-20 09:34:15 +1000

    Roots update links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,8 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path 
+      redirect_to root_path
+    else
       render :new
     end
   end
